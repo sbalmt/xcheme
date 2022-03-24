2aa0da0eb3a2ce6fe6de1df42406c58aed1f1c6b
Silas B. Domingos <silas@balmante.eti.br>
2022-03-24 13:16:11 -0300

    Small refactor

--- packages/lang/tests/maker/metadata.test.ts
+++ packages/lang/tests/maker/metadata.test.ts
@@ -20,16 +20,16 @@ test('Skip with an alias token dependency', () => {
   expect(skip.data.dependents).toHaveLength(0);
 
   // Check the resulting dependency.
-  const [alias] = skip.data.dependencies;
-  expect(alias).toBeDefined();
-  expect(alias.value).toBe(Lang.Parser.Symbols.AliasToken);
-  expect(alias.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(alias.data.exported).toBeFalsy();
-  expect(alias.data.imported).toBeFalsy();
-  expect(alias.data.identifier).toBe('ALIAS');
-  expect(alias.data.identity).toBeNaN();
-  expect(alias.data.dependencies).toHaveLength(0);
-  expect(alias.data.dependents).toHaveLength(1);
+  const [dependency] = skip.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.AliasToken);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeFalsy();
+  expect(dependency.data.identifier).toBe('ALIAS');
+  expect(dependency.data.identity).toBeNaN();
+  expect(dependency.data.dependencies).toHaveLength(0);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Skip with an imported dependency', () => {
@@ -51,16 +51,16 @@ test('Skip with an imported dependency', () => {
   expect(skip.data.dependents).toHaveLength(0);
 
   // Check the imported dependency.
-  const [imported] = skip.data.dependencies;
-  expect(imported).toBeDefined();
-  expect(imported.value).toBe(Lang.Parser.Symbols.AliasToken);
-  expect(imported.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(imported.data.exported).toBeFalsy();
-  expect(imported.data.imported).toBeTruthy();
-  expect(imported.data.identifier).toBe('EXTERNAL_TOKEN1');
-  expect(imported.data.identity).toBeNaN();
-  expect(imported.data.dependencies).toHaveLength(1);
-  expect(imported.data.dependents).toHaveLength(1);
+  const [dependency] = skip.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.AliasToken);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeTruthy();
+  expect(dependency.data.identifier).toBe('EXTERNAL_TOKEN1');
+  expect(dependency.data.identity).toBeNaN();
+  expect(dependency.data.dependencies).toHaveLength(1);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Token with an alias token dependency', () => {
@@ -82,16 +82,16 @@ test('Token with an alias token dependency', () => {
   expect(token.data.dependents).toHaveLength(0);
 
   // Check the resulting dependency.
-  const [alias] = token.data.dependencies;
-  expect(alias).toBeDefined();
-  expect(alias.value).toBe(Lang.Parser.Symbols.AliasToken);
-  expect(alias.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(alias.data.exported).toBeFalsy();
-  expect(alias.data.imported).toBeFalsy();
-  expect(alias.data.identifier).toBe('ALIAS');
-  expect(alias.data.identity).toBeNaN();
-  expect(alias.data.dependencies).toHaveLength(0);
-  expect(alias.data.dependents).toHaveLength(1);
+  const [dependency] = token.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.AliasToken);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeFalsy();
+  expect(dependency.data.identifier).toBe('ALIAS');
+  expect(dependency.data.identity).toBeNaN();
+  expect(dependency.data.dependencies).toHaveLength(0);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Token with an exported dependency', () => {
@@ -131,16 +131,16 @@ test('Token with an imported dependency', () => {
   expect(token.data.dependents).toHaveLength(0);
 
   // Check the imported dependency.
-  const [imported] = token.data.dependencies;
-  expect(imported).toBeDefined();
-  expect(imported.value).toBe(Lang.Parser.Symbols.AliasToken);
-  expect(imported.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(imported.data.exported).toBeFalsy();
-  expect(imported.data.imported).toBeTruthy();
-  expect(imported.data.identifier).toBe('EXTERNAL_TOKEN1');
-  expect(imported.data.identity).toBeNaN();
-  expect(imported.data.dependencies).toHaveLength(1);
-  expect(imported.data.dependents).toHaveLength(1);
+  const [dependency] = token.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.AliasToken);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeTruthy();
+  expect(dependency.data.identifier).toBe('EXTERNAL_TOKEN1');
+  expect(dependency.data.identity).toBeNaN();
+  expect(dependency.data.dependencies).toHaveLength(1);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Node with a loose token dependency', () => {
@@ -159,16 +159,16 @@ test('Node with a loose token dependency', () => {
   expect(node.data.dependents).toHaveLength(0);
 
   // Check the resulting loose reference.
-  const [loose] = node.data.dependencies;
-  expect(loose).toBeDefined();
-  expect(loose.value).toBe(Lang.Parser.Symbols.Token);
-  expect(loose.data.origin).toBe(Lang.Symbols.Origins.Loose);
-  expect(loose.data.exported).toBeFalsy();
-  expect(loose.data.imported).toBeFalsy();
-  expect(loose.data.identifier).toBe('@REF0');
-  expect(loose.data.identity).toBe(0);
-  expect(loose.data.dependencies).toHaveLength(0);
-  expect(loose.data.dependents).toHaveLength(1);
+  const [dependency] = node.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.Token);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.Loose);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeFalsy();
+  expect(dependency.data.identifier).toBe('@REF0');
+  expect(dependency.data.identity).toBe(0);
+  expect(dependency.data.dependencies).toHaveLength(0);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Node with an exported dependency', () => {
@@ -204,35 +204,35 @@ test('Node with an imported dependency', () => {
   expect(node.data.dependents).toHaveLength(0);
 
   // Check the imported dependency.
-  const [imported] = node.data.dependencies;
-  expect(imported).toBeDefined();
-  expect(imported.value).toBe(Lang.Parser.Symbols.AliasNode);
-  expect(imported.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(imported.data.exported).toBeFalsy();
-  expect(imported.data.imported).toBeTruthy();
-  expect(imported.data.identifier).toBe('EXTERNAL_NODE1');
-  expect(imported.data.identity).toBeNaN();
-  expect(imported.data.dependencies).toHaveLength(1);
-  expect(imported.data.dependents).toHaveLength(1);
+  const [dependency] = node.data.dependencies;
+  expect(dependency).toBeDefined();
+  expect(dependency.value).toBe(Lang.Parser.Symbols.AliasNode);
+  expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(dependency.data.exported).toBeFalsy();
+  expect(dependency.data.imported).toBeTruthy();
+  expect(dependency.data.identifier).toBe('EXTERNAL_NODE1');
+  expect(dependency.data.identity).toBeNaN();
+  expect(dependency.data.dependencies).toHaveLength(1);
+  expect(dependency.data.dependents).toHaveLength(1);
 });
 
 test('Directive importation purge', () => {
   const project = Assert.parser(`
     import './module4';`);
 
-  // Check the first token directive.
-  const token1 = project.symbols.get('EXPORTED_TOKEN1')!;
-  expect(token1).toBeDefined();
-  expect(token1.value).toBe(Lang.Parser.Symbols.Token);
-  expect(token1.data.origin).toBe(Lang.Symbols.Origins.User);
-  expect(token1.data.exported).toBeFalsy();
-  expect(token1.data.imported).toBeTruthy();
-  expect(token1.data.identity).toBe(1);
-  expect(token1.data.dependencies).toHaveLength(1);
-  expect(token1.data.dependents).toHaveLength(0);
+  // Check the token directive.
+  const token = project.symbols.get('EXPORTED_TOKEN1')!;
+  expect(token).toBeDefined();
+  expect(token.value).toBe(Lang.Parser.Symbols.Token);
+  expect(token.data.origin).toBe(Lang.Symbols.Origins.User);
+  expect(token.data.exported).toBeFalsy();
+  expect(token.data.imported).toBeTruthy();
+  expect(token.data.identity).toBe(1);
+  expect(token.data.dependencies).toHaveLength(1);
+  expect(token.data.dependents).toHaveLength(0);
 
   // Check the dependency.
-  const [dependency] = token1.data.dependencies;
+  const [dependency] = token.data.dependencies;
   expect(dependency).toBeDefined();
   expect(dependency.value).toBe(Lang.Parser.Symbols.AliasToken);
   expect(dependency.data.origin).toBe(Lang.Symbols.Origins.User);
