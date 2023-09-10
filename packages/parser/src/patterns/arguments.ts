355786f87571431594abbac8679ce285dca2c570
Silas Balmante <silas@balmante.eti.br>
2023-09-09 22:23:34 -0300

    feat(parser): allow arguments on template references

--- packages/parser/src/patterns/arguments.ts
+++ packages/parser/src/patterns/arguments.ts
@@ -4,19 +4,34 @@ import * as Lexer from '@xcheme/lexer';
 import { Nodes } from '../nodes';
 
 /**
+ * Argument operands pattern.
+ */
+const ArgumentOperands = new Core.AppendNodePattern(
+  Core.Source.Output,
+  Core.NodeDirection.Right,
+  Core.NodeDirection.Next,
+  new Core.MapFlowPattern(
+    new Core.SetValueRoute(Nodes.Identity, Lexer.Tokens.Auto),
+    new Core.SetValueRoute(Nodes.Identity, Lexer.Tokens.Number)
+  )
+);
+
+/**
+ * Argument reference pattern.
+ */
+const ArgumentReference = new Core.AppendNodePattern(
+  Nodes.Reference,
+  Core.NodeDirection.Right,
+  Core.NodeDirection.Next,
+  new Core.ExpectUnitPattern(Lexer.Tokens.Identifier),
+  new Core.OptFlowPattern(new Core.RunFlowPattern(() => Arguments))
+);
+
+/**
  * Argument list pattern.
  */
 const ArgumentList: Core.Pattern<Core.Types> = new Core.ExpectFlowPattern(
-  new Core.AppendNodePattern(
-    Core.Source.Output,
-    Core.NodeDirection.Right,
-    Core.NodeDirection.Next,
-    new Core.MapFlowPattern(
-      new Core.SetValueRoute(Nodes.Identity, Lexer.Tokens.Auto),
-      new Core.SetValueRoute(Nodes.Identity, Lexer.Tokens.Number),
-      new Core.SetValueRoute(Nodes.Reference, Lexer.Tokens.Identifier)
-    )
-  ),
+  new Core.ChooseFlowPattern(ArgumentOperands, ArgumentReference),
   new Core.OptFlowPattern(new Core.ExpectUnitPattern(Lexer.Tokens.Comma), new Core.RunFlowPattern(() => ArgumentList))
 );
 
